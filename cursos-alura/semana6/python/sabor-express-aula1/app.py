53617a19813aac0cc6cf4c88d11d7bf74bf8ec52
jcjessicarmo <jessicascarmo@terra.com.br>
2024-05-11 17:02:13 -0300

    Adicionando arquivos e exercícios da sexta semana

---
+++ cursos-alura/semana6/python/sabor-express-aula1/app.py
@@ -0,0 +1,26 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+# opcao_escolhida = input('Escolha uma opção: ')
+# print('Você escolheu a opção', opcao_escolhida)
+
+print('''
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+''')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
---
+++ cursos-alura/semana6/python/sabor-express-aula1/filosofia-python.py
@@ -0,0 +1,45 @@
+# Comando para rodar o poema
+import this
+
+# Inglês
+Beautiful is better than ugly.\
+Explicit is better than implicit.\
+Simple is better than complex.\
+Complex is better than complicated.\
+Flat is better than nested.\
+Sparse is better than dense.\
+Readability counts.\
+Special cases aren't special enough to break the rules.\
+Although practicality beats purity.\
+Errors should never pass silently.\
+Unless explicitly silenced.\
+In the face of ambiguity, refuse the temptation to guess.\
+There should be one-- and preferably only one --obvious way to do it.\
+Although that way may not be obvious at first unless you're Dutch.\
+Now is better than never.\
+Although never is often better than right now.\
+If the implementation is hard to explain, it's a bad idea.\
+If the implementation is easy to explain, it may be a good idea.\
+Namespaces are one honking great idea -- let's do more of those!\
+
+
+# Português
+Bonito é melhor que feio.\
+Explícito é melhor que implícito.\
+Simples é melhor que complexo.\
+Complexo é melhor que complicado.\
+Plano é melhor que aninhado.\
+Esparso é melhor que denso.\
+Legibilidade conta.\
+Casos especiais não são especiais o bastante para quebrar as regras.\
+Embora praticidade vença a pureza.\
+Erros nunca devem passar silenciosamente.\
+A menos que sejam explicitamente silenciados.\
+Diante da ambiguidade, recuse a tentação de adivinhar.\
+Deveria haver uma — e preferencialmente só uma — maneira óbvia de fazer algo.\
+Embora essa maneira possa não ser óbvia a princípio a menos que você seja holandês.\
+Agora é melhor que nunca.\
+Embora nunca seja frequentemente melhor que agora mesmo.\
+Se a implementação é difícil de explicar, é uma má ideia.\
+Se a implementação é fácil de explicar, pode ser uma boa ideia.\
+Namespaces são uma grande ideia — vamos fazer mais dessas!\
